2f854c4770f2d47a974bb52e5a586c361fc0622d
MarcoVernaUSI <marco.verna@usi.ch>
2020-03-13 16:22:37 +0100

    Finito Refactoring classe View

--- PresentatorSelector/src/main_package/View.java
+++ PresentatorSelector/src/main_package/View.java
@@ -31,17 +31,10 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-public class View extends JPanel
-    implements ListSelectionListener {
+public class View extends JPanel implements ListSelectionListener {
     private final Selector _selector;
-    private final ListManager _candidateList;
-    
-   
-    private final JTextField speakerName;
-    private final JButton removeButton;
-    private final JButton selectButton;
-    private final JButton addButton;
-    private final JButton logButton;
+    private final ListController _candidateList;
+    private final ButtonController _buttons;
 
     public View(Selector selector) {
         super(new BorderLayout());
@@ -50,63 +43,22 @@ public class View extends JPanel
         _selector = selector; 
         
         //Create the candidate List
-        _candidateList = new ListManager(this);
+        _candidateList = new ListController(this);
+        _buttons = new ButtonController();
         
-        // Create panel
-        JScrollPane listScrollPane = new JScrollPane(_candidateList.getList());
-        
-        
-        // Create the select speaker button
-        selectButton = new JButton("Select random speaker");
-        selectButton.addActionListener(new SelectListener());
-
-        // Create the remove speaker button
-        removeButton = new JButton("Remove speaker");
-        removeButton.addActionListener(new RemoveListener());
-
-        // Create the add speaker button
-        addButton = new JButton("Add speaker");
-        addButton.addActionListener(new AddListener());
-
-        // Print log button
-        logButton = new JButton("print Log");
-        logButton.addActionListener(new logListener());
-
-        // Create the extracted speaker box
-        speakerName = new JTextField(20);
-        speakerName.setEditable(false);
-
-        // Create a panel that uses BoxLayout.
-        JPanel buttonPane = new JPanel();
-        buttonPane.setLayout(new BoxLayout(buttonPane,
-            BoxLayout.LINE_AXIS));
-
-        buttonPane.add(removeButton);
-        buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
-        buttonPane.add(Box.createHorizontalStrut(5));
-        buttonPane.add(addButton);
-        buttonPane.add(logButton);
-        buttonPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-        JPanel buttonPane2 = new JPanel();
-        buttonPane2.setLayout(new BoxLayout(buttonPane2,
-            BoxLayout.LINE_AXIS));
-
-        buttonPane2.add(selectButton);
-        buttonPane2.add(speakerName);
-        buttonPane2.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
+        // Create panels
+        JScrollPane listScrollPane = _candidateList.createPanel();
+        JPanel buttonPane1 = _buttons.createUpperPanel();
+        JPanel buttonPane2 = _buttons.createBottomPanel();
         JLabel description = new JLabel("right click for toggle/untoggle absent status");
         
         add(description,BorderLayout.AFTER_LINE_ENDS);
         add(listScrollPane, BorderLayout.PAGE_START);
-        add(buttonPane, BorderLayout.CENTER);
+        add(buttonPane1, BorderLayout.CENTER);
         add(buttonPane2, BorderLayout.PAGE_END);
         
-        _candidateList.updateList();
+        updateList();
     }
-
-    
 
     // This method is required by ListSelectionListener.
     @Override
@@ -114,64 +66,30 @@ public class View extends JPanel
         if (e.getValueIsAdjusting() == false) {
             if (_candidateList.getList().getSelectedIndex() == -1) {
                 // No selection, disable remove button.
-                removeButton.setEnabled(false);
+                _buttons.abilitateRemoveButton(false);
             } else {
                 // Selection, enable the remove button.
-                removeButton.setEnabled(true);
+                _buttons.abilitateRemoveButton(true);
             }
         }
     }
-
-    // Listeners
-    class SelectListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            speakerName.setText(_selector.select());
-        }
-    }
-
-    class RemoveListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // get selected index
-            String speaker = _candidateList.getList().getSelectedValue();  
-            _selector.remove(speaker);  
-            _candidateList.updateList();
-        }
-    }
-
-    class logListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.out.println(_selector.printLog());
-        }
-    }
-
-    class AddListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            new AddFrame(_candidateList);
-            addButton.setEnabled(false);
-        }
+    
+    public void updateList() {
+        _candidateList.updateList();
     }
     
-    
-    
-    
-    
-    
-    
+    public JList<String> getCandidateList() {
+        return _candidateList.getList();
+    }
 
-    
-    public class ListManager {
+    // List Panel
+    private class ListController {
         private final JList<String> candidateList;
         private final DefaultListModel<String> candidateListModel;
         
-        public ListManager (View view) {
-                        
-        
+        public ListController (View view) {
             candidateListModel = new DefaultListModel<>();
-
+            
             // Create the list and put it in a scroll pane.
             candidateList = new JList<>(candidateListModel);
             candidateList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -182,8 +100,6 @@ public class View extends JPanel
             
             // add mouse listener for absents
             candidateList.addMouseListener(new AbsentListener());
-        
-            
         }
 
         // Consistency between view and model
@@ -193,19 +109,22 @@ public class View extends JPanel
                 candidateListModel.addElement(speaker);
             }
             if (candidateListModel.getSize() == 0) {  
-                selectButton.setEnabled(false);
+                _buttons.abilitateSelectButton(false);
             }
-            if (candidateListModel.getSize() > 0 & !selectButton.isEnabled()) {  
-                selectButton.setEnabled(true);
+            if (candidateListModel.getSize() > 0 & !_buttons.isSelectButtonEnabled()) {  
+                _buttons.abilitateSelectButton(true);
             }
         }
-            
+        
+        public JScrollPane createPanel() {
+            return new JScrollPane(getList());
+        }
+        
         public JList<String> getList() {
             return candidateList;
         }
         
         class AbsentListener extends MouseAdapter {
-            
             @Override
             public void mousePressed(MouseEvent e) {
                 if ( SwingUtilities.isRightMouseButton(e) ) {      
@@ -224,42 +143,171 @@ public class View extends JPanel
                  }
              }
         }
-
+        
+        class MyListCellRenderer extends JLabel implements ListCellRenderer {
+            public MyListCellRenderer() {
+                setOpaque(true);
+            }
+            @Override
+            public Component getListCellRendererComponent(JList paramlist, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                setText(value.toString());
+                if (_selector.checkAbsent(value.toString())) {
+                    setBackground(Color.RED);
+                } else
+                    setBackground(Color.white);
+                if (isSelected) {
+                    setBackground(getBackground().darker());
+               }
+                return this;
+            }
+        }
     }
     
-    class MyListCellRenderer extends JLabel implements ListCellRenderer {
-        public MyListCellRenderer() {
-            setOpaque(true);
+
+    private class ButtonController {
+        private final JTextField speakerName;
+        private final JButton removeButton;
+        private final JButton selectButton;
+        private final JButton addButton;
+        private final JButton logButton;
+
+        public ButtonController() {
+            // Create the select speaker button
+            selectButton = new JButton("Select random speaker");
+            selectButton.addActionListener(new SelectListener());
+
+            // Create the remove speaker button
+            removeButton = new JButton("Remove speaker");
+            removeButton.addActionListener(new RemoveListener());
+
+            // Create the add speaker button
+            addButton = new JButton("Add speaker");
+            addButton.addActionListener(new AddListener());
+
+            // Print log button
+            logButton = new JButton("print Log");
+            logButton.addActionListener(new logListener());
+
+            // Create the extracted speaker box
+            speakerName = new JTextField(20);
+            speakerName.setEditable(false);
         }
-        @Override
-        public Component getListCellRendererComponent(JList paramlist, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            setText(value.toString());
-            if (_selector.checkAbsent(value.toString())) {
-                setBackground(Color.RED);
-            } else
-                setBackground(Color.white);
-            if (isSelected) {
-                setBackground(getBackground().darker());
-           }
-            return this;
+        
+        public JPanel createUpperPanel() {
+         // Create a panel that uses BoxLayout.
+            JPanel buttonPane = new JPanel();
+            buttonPane.setLayout(new BoxLayout(buttonPane,
+                BoxLayout.LINE_AXIS));
+            buttonPane.add(removeButton);
+            buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
+            buttonPane.add(Box.createHorizontalStrut(5));
+            buttonPane.add(addButton);
+            buttonPane.add(logButton);
+            buttonPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            return buttonPane;
+        }
+        
+        public JPanel createBottomPanel() {
+            JPanel buttonPane = new JPanel();
+            buttonPane.setLayout(new BoxLayout(buttonPane,
+                BoxLayout.LINE_AXIS));
+            buttonPane.add(selectButton);
+            buttonPane.add(speakerName);
+            buttonPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            return buttonPane;
+        }
+        
+        public void abilitateRemoveButton(boolean flag) {
+            removeButton.setEnabled(flag);    
+        }
+        
+        public void abilitateSelectButton(boolean flag) {
+            selectButton.setEnabled(flag);    
+        }
+        
+        public void abilitateAddButton(boolean flag) {
+            addButton.setEnabled(flag);    
+        }
+        
+        public boolean isSelectButtonEnabled() {
+            return selectButton.isEnabled();
+        }
+        
+        
+        // Buttons Listeners
+        class SelectListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                speakerName.setText(_selector.select());
+            }
+        }
+
+        class RemoveListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // get selected index
+                String speaker = _candidateList.getList().getSelectedValue();  
+                _selector.remove(speaker);  
+                _candidateList.updateList();
+            }
+        }
+
+        class logListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(_selector.printLog());
+            }
+        }
+
+        class AddListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new AddFrame(_candidateList);
+                addButton.setEnabled(false);
+            }
         }
     }
-    
-
-    
-    
-    
-    
-    
-    
 
     // Add Frame
-    class AddFrame {
-        private final JFrame f = new JFrame("Add");
+    private class AddFrame {
+        private final JFrame _frame = new JFrame("Add");
         private final JButton innerAddButton;
         private final JTextField fnameField;
         private final JTextField surnameField;
-        private final ListManager _candidatesList;
+        private final ListController _candidatesList;
+        
+        public AddFrame(ListController candidatesList) {
+            _frame.setTitle("Add candidate");
+            _frame.setBounds(20, 20, 300, 150);
+            _frame.setResizable(false);
+            _frame.setVisible(true);
+            _frame.addWindowListener(new closeListener());
+            _candidatesList = candidatesList;
+
+            // Create the add speaker button
+            this.innerAddButton = new JButton("Add speaker");
+            innerAddButton.addActionListener(new innerAddListener());
+
+            this.fnameField = new JTextField(20);
+            this.surnameField = new JTextField(20);
+
+            _frame.getContentPane().add(createInnerPanel());
+        }
+        
+     // Create a panel that uses BoxLayout.
+        private JPanel createInnerPanel() {
+            JPanel innerPane = new JPanel();
+            innerPane.setLayout(new BoxLayout(innerPane,
+                BoxLayout.Y_AXIS));
+            innerPane.add(new JLabel("Name:"));
+            innerPane.add(this.fnameField);
+            innerPane.add(new JLabel("Surname:"));
+            innerPane.add(this.surnameField);
+            innerPane.add(this.innerAddButton);
+            innerPane.add(Box.createHorizontalStrut(5));
+            innerPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            return innerPane;
+        }
 
         class innerAddListener implements ActionListener {
             @Override
@@ -271,50 +319,18 @@ public class View extends JPanel
                     _selector.add(fname, surname);
                     _candidatesList.updateList();
                 }
-                addButton.setEnabled(true);
-                f.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-                f.dispose();
+                _buttons.abilitateAddButton(true);
+                _frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+                _frame.dispose();
             }
         }
 
         class closeListener extends WindowAdapter {
             @Override
             public void windowClosing(WindowEvent e) {
-                addButton.setEnabled(true);
+                _buttons.abilitateAddButton(true);
             }
         }
-
-        public AddFrame(ListManager candidatesList) {
-            f.setTitle("Add candidate");
-            f.setBounds(20, 20, 300, 150);
-            f.setResizable(false);
-            f.setVisible(true);
-            f.addWindowListener(new closeListener());
-            _candidatesList = candidatesList;
-
-            // Create the add speaker button
-            this.innerAddButton = new JButton("Add speaker");
-            innerAddButton.addActionListener(new innerAddListener());
-
-            this.fnameField = new JTextField(20);
-            this.surnameField = new JTextField(20);
-
-            // Create a panel that uses BoxLayout.
-            JPanel innerPane = new JPanel();
-            innerPane.setLayout(new BoxLayout(innerPane,
-                BoxLayout.Y_AXIS));
-
-            innerPane.add(new JLabel("Name:"));
-            innerPane.add(this.fnameField);
-            innerPane.add(new JLabel("Surname:"));
-            innerPane.add(this.surnameField);
-            innerPane.add(this.innerAddButton);
-            innerPane.add(Box.createHorizontalStrut(5));
-            innerPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-            f.getContentPane().add(innerPane);
-        }
-
     }
 
 }
