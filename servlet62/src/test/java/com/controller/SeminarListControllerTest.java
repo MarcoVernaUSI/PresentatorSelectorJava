44f3fb94dbbb72b8fb8b2e444c45ab7479d6c383
MarcoVernaUSI <marco.verna@usi.ch>
2020-04-07 09:43:30 +0200

    FInito refactoring, mancano un paio di test

--- servlet62/src/test/java/com/controller/SeminarListControllerTest.java
+++ servlet62/src/test/java/com/controller/SeminarListControllerTest.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+
 public class SeminarListControllerTest {
 
 	@Test
 	public void handlesRoute() throws Exception {
 		assertTrue(new SeminarListController().handles("/"));
-	}	
+	}
+	
+//	@Test
+ //   public void executeTest() throws Exception {
+  //      FakeHttpServletRequest request = new FakeHttpServletRequest("/");
+ //       FakeHttpServletResponse response = new FakeHttpServletResponse();
+        //Iterable<Seminar> seminars = new SeminarMapper(context.connection()).findAll();
+
+        
+
+//        assertThat(response.statusCode(), is(HttpServletResponse.SC_OK));
+  //      assertThat(response.message(), containsString("form"));
+ //   }
+
 	
 }
